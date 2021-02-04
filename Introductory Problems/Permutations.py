a94f63ffd0269c054d17a4d46e6ce7366a61fb69
Divyansh Chaudhary <divyansh7c@gmail.com>
2021-02-04 23:49:58 +0530

    Add Permutations Solution

--- Introductory Problems/Permutations.py
+++ Introductory Problems/Permutations.py
@@ -3,10 +3,10 @@ all_even = []
 all_odd = []
 if n == 1:
   print(1)
-elif n <=3:
+elif n <= 3:
   print("NO SOLUTION")
 else:
-  for i in range(1,n+1):
+  for i in range(1,n + 1):
     if i % 2 == 0:
       all_even.append(i)
     else:
