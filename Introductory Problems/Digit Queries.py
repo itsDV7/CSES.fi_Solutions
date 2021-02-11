31af350b427d74d99f1c4b931e18907c48308482
Divyansh Chaudhary <divyansh7c@gmail.com>
2021-02-11 20:35:07 +0530

    Add Digit Queries Solution

--- Introductory Problems/Digit Queries.py
+++ Introductory Problems/Digit Queries.py
@@ -4,8 +4,8 @@ def findNthDigit(n):
                 n -= 9*base*digit
                 digit += 1
                 base *= 10 
-        q, r = divmod(n-1, digit)
-        return int(str(base + q)[r])
+        p, q = divmod(n-1, digit)
+        return int(str(base + p)[q])
  
 for _ in range(int(input())):
         n = int(input())
