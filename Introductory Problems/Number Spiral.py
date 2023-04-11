4752b333b8bf3501b75ddb653dbde4428a9e938b
Divyansh Chaudhary <divyansh7c@gmail.com>
2023-04-11 23:40:10 +0530

    Update Number Spiral.py

--- Introductory Problems/Number Spiral.py
+++ Introductory Problems/Number Spiral.py
@@ -1,23 +1,24 @@
 t = int(input())
-ans = []
-for i in range(t):
-  row, col = list(map(int,input().strip().split()))
-  if row % 2 == 0:
-    row_start = row **2
-  else:
-    row_start = ((row - 1) **2) + 1
-  if col % 2 != 0:
-    col_start = col **2
-  else:
-    col_start = ((col - 1) **2) + 1
+while t:
+  ans = []
+  for i in range(t):
+    row, col = list(map(int,input().strip().split()))
+    if row % 2 == 0:
+      row_start = row **2
+    else:
+      row_start = ((row - 1) **2) + 1
+    if col % 2 != 0:
+      col_start = col **2
+    else:
+      col_start = ((col - 1) **2) + 1
 
-  if col >= row and col_start % 2 == 0:
-    ans.append(col_start + row - 1)
-  elif col > row and col_start % 2 != 0:
-    ans.append(col_start - row + 1)
-  elif col < row and row_start % 2 == 0:
-    ans.append(row_start - col + 1)
-  else:
-    ans.append(row_start + col - 1)
-
-print(*ans)
+    if col >= row and col_start % 2 == 0:
+      ans.append(col_start + row - 1)
+    elif col > row and col_start % 2 != 0:
+      ans.append(col_start - row + 1)
+    elif col < row and row_start % 2 == 0:
+      ans.append(row_start - col + 1)
+    else:
+      ans.append(row_start + col - 1)
+  print(*ans)
+  t -= 1
